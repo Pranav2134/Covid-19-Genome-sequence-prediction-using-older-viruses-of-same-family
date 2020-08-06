8e5d10da4a857036a7e528b45e04c6a3b9dfa62a
Pranav Pushkar <koolpranav1996@gmail.com>
2020-08-06 10:19:58 +0530

    Add files via upload

---
+++ token_queries.sql
@@ -0,0 +1,21 @@
+show databases;
+create database project;
+use project;
+show tables;
+create table token(
+char1 INT,char2 INT,char3 INT,char4 INT,char5 INT,char6 INT,char7 INT,
+char8 INT,char9 INT,char10 INT,char11 INT,char12 INT,char13 INT,char14 INT,
+char15 INT,char16 INT,char17 INT,char18 INT,char19 INT,char20 INT,char21 INT,
+char22 INT,char23 INT,char24 INT,char25 INT,char26 INT,char27 INT,char28 INT,
+char29 INT,char30 INT,char31 INT,char32 INT,char33 INT,char34 INT,char35 INT,
+char36 INT,char37 INT,char38 INT,char39 INT,char40 INT,char41 INT,char42 INT,
+char43 INT,char44 INT,char45 INT,char46 INT,char47 INT,char48 INT,char49 INT,
+char50 INT,char51 INT,char52 INT,char53 INT,char54 INT,char55 INT,char56 INT,
+char57 INT,char58 INT,char59 INT,char60 INT,char61 INT,char62 INT,char63 INT,
+char64 INT,char65 INT,char66 INT,char67 INT,char68 INT,char69 INT,char70 INT
+);
+describe token;
+insert into token values(
+
+);
+select * from token;
